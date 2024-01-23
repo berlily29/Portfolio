4ed48b3d099c1ca834f72cd8df43c31fe6cc4210
berlily29 <yambaoluise29@gmail.com>
2024-01-24 00:09:16 +0800

    window reload modification

--- js/index.js
+++ js/index.js
@@ -1,37 +1,32 @@
 
-
-
-
-
-
-
-
 //for menu
 var menu =  document.querySelector('.menu');
 var navbar = document.querySelector('.navbar');
 var menuimg = document.querySelector('.menu2');
 var toggleMenu = true;
 
+window.addEventListener('resize',()=> {location.reload()});
 
-
-menu.addEventListener("click", ()=> {
-    if (toggleMenu == true ) {
+menu.addEventListener("click", () =>{
+    if (toggleMenu == true) {
         toggleMenu = false
         navbar.style.display = 'flex';
+        
 
         setTimeout(() => {
             menuimg.src = 'css/assets/exit.png';
             navbar.style.opacity = '1';
+            navbar.style.height ='80vh';
     
         }, 100);
 
     }
     else if (toggleMenu == false) {
         toggleMenu = true;
-        navbar.style.opacity = '0';
+
         menuimg.src = 'css/assets/menu.png'
         setTimeout(() => {
-            navbar.style.display = 'none';
+            navbar.style.height ='0vh';
             }, 100);
 
     }
@@ -40,4 +35,3 @@ menu.addEventListener("click", ()=> {
     }
 
 });
-
