db1b207ed53466437880879d254f95ee1bc3042b
berlily29 <yambaoluise29@gmail.com>
2024-01-24 17:56:47 +0800

    added feature

--- js/index.js
+++ js/index.js
@@ -4,8 +4,17 @@ var menu =  document.querySelector('.menu');
 var navbar = document.querySelector('.navbar');
 var menuimg = document.querySelector('.menu2');
 var toggleMenu = true;
+var header1 = document.querySelector('.header1');
+var header2 = document.querySelector('.header2');
+
 
 window.addEventListener('resize',()=> {location.reload()});
+
+window.addEventListener('load', ()=> {
+    header1.style.opacity = '1';
+    header2.style.opacity = '1';
+
+})
 
 menu.addEventListener("click", () =>{
     if (toggleMenu == true) {
@@ -35,3 +44,20 @@ menu.addEventListener("click", () =>{
     }
 
 });
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
